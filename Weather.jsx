9e4829554a67f086d7d3a8682437a4a61aa0094c
Hatim <hatimmurtuza10@gmail.com>
2020-09-26 23:05:21 +0300

    Add loading and weather screens

---
+++ Weather.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import PropTypes from 'prop-types';
+import { LinearGradient } from 'expo-linear-gradient';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { weatherOptions } from './weatherOptions';
+
+export default function Weather({ temp, condition }) {
+	return (
+		<LinearGradient
+			colors={weatherOptions[condition].gradient}
+			style={styles.container}>
+			<StatusBar barStyle='light-content' />
+			<View style={styles.halfContainer}>
+				<MaterialCommunityIcons
+					size={96}
+					name={weatherOptions[condition].iconName}
+					color='white'
+				/>
+				<Text style={styles.temp}>{temp}°</Text>
+			</View>
+			<View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+				<Text style={styles.title}>
+					{weatherOptions[condition].title}
+				</Text>
+				<Text style={styles.subtitle}>
+					{weatherOptions[condition].subtitle}
+				</Text>
+			</View>
+		</LinearGradient>
+	);
+}
+
+Weather.propTypes = {
+	temp: PropTypes.number.isRequired,
+	condition: PropTypes.oneOf([
+		'Thunderstorm',
+		'Drizzle',
+		'Rain',
+		'Snow',
+		'Atmosphere',
+		'Clear',
+		'Clouds',
+		'Haze',
+		'Mist',
+		'Dust',
+	]).isRequired,
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	temp: {
+		fontSize: 42,
+		color: 'white',
+	},
+	halfContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	title: {
+		color: 'white',
+		fontSize: 44,
+		fontWeight: '300',
+		marginBottom: 10,
+	},
+	subtitle: {
+		fontWeight: '600',
+		color: 'white',
+		fontSize: 24,
+	},
+	textContainer: {
+		paddingHorizontal: 20,
+		alignItems: 'flex-start',
+	},
+});
---
+++ weatherOptions.js
@@ -0,0 +1,60 @@
+export const weatherOptions = {
+	Thunderstorm: {
+		iconName: 'weather-lightning',
+		gradient: ['#373B44', '#4286f4'],
+		title: 'Thunderstorm',
+		subtitle: 'IT’S RAINING MONKEYS',
+	},
+	Drizzle: {
+		iconName: 'weather-hail',
+		gradient: ['#89F7FE', '#66A6FF'],
+		title: 'Drizzle',
+		subtitle: '',
+	},
+	Rain: {
+		iconName: 'weather-rainy',
+		gradient: ['#00C6FB', '#005BEA'],
+		title: 'Raining',
+		subtitle: 'For more info look outside',
+	},
+	Snow: {
+		iconName: 'weather-snowy',
+		gradient: ['#7DE2FC', '#B9B6E5'],
+		title: 'Cold',
+		subtitle: 'No Netflix, just chill.',
+	},
+	Atmosphere: {
+		iconName: 'weather-hail',
+		gradient: ['#89F7FE', '#66A6FF'],
+	},
+	Clear: {
+		iconName: 'weather-sunny',
+		gradient: ['#FEF253', '#FF7300'],
+		title: 'A clear day',
+		subtitle: 'Go back to bed',
+	},
+	Clouds: {
+		iconName: 'weather-cloudy',
+		gradient: ['#D7D2CC', '#304352'],
+		title: 'Clouds',
+		subtitle: 'I know, damn boring',
+	},
+	Mist: {
+		iconName: 'weather-hail',
+		gradient: ['#4DA0B0', '#D39D38'],
+		title: 'Mist!',
+		subtitle: "It's like you have no glasses on.",
+	},
+	Dust: {
+		iconName: 'weather-hail',
+		gradient: ['#4DA0B0', '#D39D38'],
+		title: 'Dusty',
+		subtitle: 'Thanks a lot China',
+	},
+	Haze: {
+		iconName: 'weather-hail',
+		gradient: ['#4DA0B0', '#D39D38'],
+		title: 'Haze',
+		subtitle: "Just don't go outside.",
+	},
+};
